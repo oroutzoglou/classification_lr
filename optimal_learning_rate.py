a83d3380f35fca281b476b7f10e07c3e8fdc05f7
yoroutzoglou <ioroutzoglou@gmail.com>
2020-11-13 11:56:20 +0200

    Initial

--- optimal_learning_rate.py
+++ optimal_learning_rate.py
@@ -11,16 +11,12 @@ train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_datas
 # Example of a picture
 index = 26
 plt.imshow(train_set_x_orig[index])
-#plt.show()
+plt.show()
 print ("y = " + str(train_set_y[:, index]) + ", it's a '" + classes[np.squeeze(train_set_y[:, index])].decode("utf-8") +  "' picture.")
 
-####################################################
-### START CODE HERE ### (≈ 3 lines of code)
-### START CODE HERE ### (≈ 3 lines of code)
 m_train =  train_set_x_orig.shape[0]
 m_test =  test_set_x_orig.shape[0]
 num_px =  train_set_x_orig.shape[1]
-### END CODE HERE ###
 
 print ("Number of training examples: m_train = " + str(m_train))
 print ("Number of testing examples: m_test = " + str(m_test))
@@ -31,14 +27,11 @@ print ("train_set_y shape: " + str(train_set_y.shape))
 print ("test_set_x shape: " + str(test_set_x_orig.shape))
 print ("test_set_y shape: " + str(test_set_y.shape))
 
-#print(train_set_x_orig.reshape(m_train*num_px*num_px*3, 1).shape)
 
 # Reshape the training and test examples
 
-### START CODE HERE ### (≈ 2 lines of code)
 train_set_x_flatten = train_set_x_orig.reshape(train_set_x_orig.shape[0], -1).T
 test_set_x_flatten = test_set_x_orig.reshape(test_set_x_orig.shape[0], -1).T
-### END CODE HERE ###
 
 print ("train_set_x_flatten shape: " + str(train_set_x_flatten.shape))
 print ("train_set_y shape: " + str(train_set_y.shape))
