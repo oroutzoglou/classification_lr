994def0abfea0b32271c377a291bcaf7cb6b23d4
yoroutzoglou <ioroutzoglou@gmail.com>
2020-11-13 09:58:00 +0200

    Initial

--- model.py
+++ model.py
@@ -9,22 +9,16 @@ from pyfunctions import predict
 
 
 def model(X_train, Y_train, X_test, Y_test, num_iterations = 2000, learning_rate = 0.5, print_cost = False):
-    """
-    Builds the logistic regression model by calling the function you've implemented previously
-    
-    Arguments:
-    X_train -- training set represented by a numpy array of shape (num_px * num_px * 3, m_train)
-    Y_train -- training labels represented by a numpy array (vector) of shape (1, m_train)
-    X_test -- test set represented by a numpy array of shape (num_px * num_px * 3, m_test)
-    Y_test -- test labels represented by a numpy array (vector) of shape (1, m_test)
-    num_iterations -- hyperparameter representing the number of iterations to optimize the parameters
-    learning_rate -- hyperparameter representing the learning rate used in the update rule of optimize()
-    print_cost -- Set to true to print the cost every 100 iterations
-    
-    Returns:
-    d -- dictionary containing information about the model.
-    """
-    
+   # builds the logistic regression model
+   # X_train -- training set represented by a numpy array of shape (num_px * num_px * 3, m_train)
+   # Y_train -- training labels represented by a numpy array (vector) of shape (1, m_train)
+   # X_test -- test set represented by a numpy array of shape (num_px * num_px * 3, m_test)
+   # Y_test -- test labels represented by a numpy array (vector) of shape (1, m_test)
+   # num_iterations -- hyperparameter representing the number of iterations to optimize the parameters
+   # learning_rate -- hyperparameter representing the learning rate used in the update rule of optimize()
+   # print_cost -- Set to true to print the cost every 100 iterations    
+   # returns:
+   # d -- dictionary containing information about the model.
     
     # initialize parameters with zeros (≈ 1 line of code)
     w, b = initialize_with_zeros(X_train.shape[0])
@@ -40,11 +34,9 @@ def model(X_train, Y_train, X_test, Y_test, num_iterations = 2000, learning_rate
     Y_prediction_test = predict(w,b,X_test)
     Y_prediction_train = predict(w,b,X_train)
 
-
     # Print train/test Errors
     print("train accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_train - Y_train)) * 100))
     print("test accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_test - Y_test)) * 100))
-
     
     d = {"costs": costs,
          "Y_prediction_test": Y_prediction_test, 
